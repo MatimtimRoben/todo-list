f4f2039a1d4dff8e89602e742f9ecb7672e5668c
roben <asakatol1234@gmail.com>
2023-09-27 10:00:31 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require ("express");
+const app = express();
+let items = ["Wake up","Eat","Code","Rest"];
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.get("/", function(req, res){
+let today = new Date ();
+let myDate = {
+ weekday: "long",
+ day: "numeric",
+ month: "long"
+};
+let day = today.toLocaleDateString("en-US", myDate)
+
+res.render("list", {myDay: day, addedItem: items});
+
+});
+app.post("/", function(req, res){
+// item = req.body.addItem;
+var item = req.body.addItem;
+items.push(item);
+ res.redirect("/");
+})
+
+app.listen(8000, function(){
+ console.log("Server started on port 8000")
+})
